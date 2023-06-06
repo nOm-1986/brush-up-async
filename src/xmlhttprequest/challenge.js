5a8cb64d3d20218a099df29eb1c600c34940e332
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2023-06-06 15:07:59 -0500

    The package.json was modified, now you can run node run plus path to run scripts

--- src/xmlhttprequest/challenge.js
+++ src/xmlhttprequest/challenge.js
@@ -17,20 +17,25 @@ const fetchData = (urlApi, callback) => {
     xhttp.send();
 }
 
-// fetchData(`${API}/products`, function(error1, data1){
-//     if(error1) return console.error(error1);
-//         fetchData(`${API}/products/${data1[0].id}`, function (error2, data2) {
-//             if(error2) return console.error(error2);
-//             fetchData(`${API}/categories/${data2?.category?.id}`, function (error3, data3) {
-//                 if(error3) return console.error(error3);
-//                 console.log('data1: ', data1[0]);
-//                 console.log('data2: ', data2.title);
-//                 console.log('data3: ', data3.name);
-//             });
-//         })
-// })
+const requestOne = () => { 
+    fetchData(`${API}/products`, function(error1, data1){
+        if(error1) return console.error(error1);
+            fetchData(`${API}/products/${data1[0].id}`, function (error2, data2) {
+                if(error2) return console.error(error2);
+                fetchData(`${API}/categories/${data2?.category?.id}`, function (error3, data3) {
+                    if(error3) return console.error(error3);
+                    console.log('data1: ', data1[0]);
+                    console.log('data2: ', data2.title);
+                    console.log('data3: ', data3.name);
+                });
+            })
+    });
+}
 
-fetchData(`${API}/products?offset=0&limit=10`, (error1, data1) => {
-    if(error1) return console.error(error1);
-    console.log(data1);
-});
+const requestTwo = () => { 
+    fetchData(`${API}/products?offset=0&limit=5`, (error1, data1) => {
+        if(error1) return console.error(error1);
+        console.log(data1);
+    });
+}
+requestTwo();
