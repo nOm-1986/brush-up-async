a13933c6eed6732171107d2412e0f5d1fc63fc24
Fabian Beltran <joshefbeltran@gmail.com>
2023-06-28 16:53:38 -0500

    All done

---
+++ 18-APIRestFundamentals/05-fetch-properties.js
@@ -0,0 +1,17 @@
+/**
+ * Mode: -cors, -no-cors, same-origin,
+    * Limita con quien intercambia o no la información
+    * same-origin: solamente el mismo origin puede hacer solicitudes a la API.
+    * cors: podemos decir con quien podemos intercambiar información.
+    * no-cors: permiteme hacer las soclicitudes a donde yo quiera.
+ *  
+ * 
+ * Caché:
+ *    Capacidad de recordar la información de una solicitud anterior, guardandola en cache.
+ *    default, no-store, reload, no-cache, force-cache, only-if-cached.
+ * 
+ * 
+ * Redirec
+ *    Que hacer cuando nos encontramos con un status.code == 300 -> redirect.
+ *    follow, error, manual.
+ */
---
+++ 18-APIRestFundamentals/06-mas-alla-de-rest.js
@@ -0,0 +1,6 @@
+/**
+ * sendBeacon
+ * GraphQL -> Para no tener distintos endpoints. ---->>>>> ,-('-')-, Complica al backend.
+ * Web Sockets - Dejar el "tulen abierto". Útil para aplicaciones real-time.
+ * Dapps: Aplicaciones descentralizadas.
+*/ 
